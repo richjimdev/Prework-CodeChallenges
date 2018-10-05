dbe17ead4e144136c53a063d08a6cbcfcdade20f
Richard J <richjim89@gmail.com>
2018-10-04 20:52:08 -0700

    Add project files.

---
+++ PreworkChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PreworkChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
