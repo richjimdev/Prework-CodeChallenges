b3ce76dd811c8550b5ae39da4c703170134202c8
Richard J <richjim89@gmail.com>
2018-10-04 22:23:21 -0700

    Testing challenge 3

--- PreworkChallenges/Program.cs
+++ PreworkChallenges/Program.cs
@@ -6,8 +6,12 @@ namespace PreworkChallenges
     {
         static void Main(string[] args)
         {
-            ChallengeOne();
-            ChallengeTwo();
+            //ChallengeOne();
+            //ChallengeTwo();
+            Console.WriteLine($"Testing [1, 3, 2]. Answer: {ChallengeThree(new int[] { 1, 3, 2 })}");
+            Console.WriteLine($"Testing [0, 0, 0, 0]. Answer: {ChallengeThree(new int[] { 0, 0, 0, 0 })}");
+            Console.WriteLine($"Testing [4, 5, 6]. Answer: {ChallengeThree(new int[] { 4, 5, 6 })}");
+            Console.WriteLine($"Testing [0, 2, -2]. Answer: {ChallengeThree(new int[] { 0, 2, -2 })}");
             Console.ReadLine();
         }
 
@@ -55,6 +59,20 @@ namespace PreworkChallenges
                     Console.WriteLine(correct);
             } else
                 Console.WriteLine(incorrect);
+        }
+
+        static string ChallengeThree(int[] arr)
+        {
+            int sum = 0;
+            int product = 1;
+            foreach (int num in arr)
+            {
+                if (num < 0) return "no";
+                sum += num;
+                product *= num;
+            }
+            if (sum == product) return "yes";
+            return "no";
         }
     }
 }
