3155b2cd4c8652d6ae953e5db978c2fb88121e90
Richard J <richjim89@gmail.com>
2018-10-04 21:00:23 -0700

    Created random numbers array

--- PreworkChallenges/Program.cs
+++ PreworkChallenges/Program.cs
@@ -6,7 +6,19 @@ namespace PreworkChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ChallengeOne();
+        }
+
+        static void ChallengeOne()
+        {
+            Random rng = new Random();
+            int[] randArray = { rng.Next(1, 10),
+                rng.Next(1, 10),
+                rng.Next(1, 10),
+                rng.Next(1, 10),
+                rng.Next(1, 10) };
+            Console.WriteLine(string.Join(", ", randArray));
+            Console.ReadLine();
         }
     }
 }
